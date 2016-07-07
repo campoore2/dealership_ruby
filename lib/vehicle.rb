1265cd90a0595b292ec52cf12a663080eaeb54b6
Caleb Stokka and Cameron Poore <caleb+cameron@>
2016-07-07 08:46:08 -0700

    passes 18 back end specs

---
+++ lib/vehicle.rb
@@ -0,0 +1,47 @@
+class Vehicle
+  @@vehicles = []
+
+  attr_reader(:make, :model, :year, :color, :engine_size, :number_of_doors, :id)
+
+  define_method(:initialize) do |attributes|
+    @make = attributes.fetch(:make)
+    @model = attributes.fetch(:model)
+    @year = attributes.fetch(:year).to_i
+    @color = attributes.fetch(:color)
+    @engine_size= attributes.fetch(:engine_size)
+    @number_of_doors = attributes.fetch(:number_of_doors)
+    @id = @@vehicles.length().+(1)
+  end
+
+  define_singleton_method(:all) do
+    @@vehicles
+  end
+
+  define_method(:save) do
+    @@vehicles.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@vehicles = []
+  end
+
+  define_method(:age) do
+    current_year = Time.new().year()
+    current_year.-(@year)
+  end
+
+  define_method(:worth_buying?) do
+    american_cars = ["Chrysler", "Ford", "GM"]
+    american_cars.include?(@make).&(self.age().<=(15))
+  end
+
+  define_singleton_method(:find) do |identification|
+    found_vehicle = nil
+    @@vehicles.each() do |vehicle|
+      if vehicle.id().eql?(identification.to_i())
+        found_vehicle = vehicle
+      end
+    end
+    found_vehicle
+  end
+end
